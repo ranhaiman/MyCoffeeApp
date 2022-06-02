39f0209f4efa66857f37de5a1dc164d4bbfafed5
Ran <ran.haiman@yahoo.com>
2022-06-03 00:19:14 +0100

    namespace clean

--- MyCoffeeApp.WebAPI/Controllers/CoffeeController.cs
+++ MyCoffeeApp.WebAPI/Controllers/CoffeeController.cs
@@ -1,9 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MyCoffeeApp.Shared.Models;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
